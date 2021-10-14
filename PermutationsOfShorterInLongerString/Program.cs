d696bdc2b180f2111c510798e697fd095fc96350
Ronald Bett <rbett@jambopay.com>
2021-10-14 07:49:19 +0300

    Initial commit

---
+++ PermutationsOfShorterInLongerString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PermutationsOfShorterInLongerString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
